728f95759cc31c366d1f6c2b97842ff7725997bd
Pop Robert <poprobert1999@yahoo.com>
2025-01-16 14:18:50 +0200

    Second day  of learning for the ISTQB exam

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 """
-First Day of learning ISTQB ecam Chapter 1
-
+First Day of learning ISTQB exam Chapter 1
+I learnt about what is testing and test objectives
 
 """
+
+"""
+Day 2  of learning ISTQB exam Chapter 1
+I Learnt about testing and debugging, why is testing necessary, testings contribution to success
+Errors, defect, failures and root causes and did some exam tests
+"""
+
