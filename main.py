4460435ecc657207b00e219ec734c60c900edf8f
Pop Robert <poprobert1999@yahoo.com>
2025-01-21 13:37:13 +0200

    5th day  of learning for the ISTQB exam

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ Day 4 of learing for ISTQB exam chapter 1
 i did a lot of exam questions like 100
 learnt about the errors and test features
 """
+
+"""
+Day 5  of learing for ISTQB exam chapter 1
+I did a lot of exam questions and learned about test activities, testware and test roles
+"""
