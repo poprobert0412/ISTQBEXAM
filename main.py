843c9bc450921adceead69d7ca90e238dea5f0f3
Pop Robert <poprobert1999@yahoo.com>
2025-01-28 16:18:48 +0200

    7th day  of learning for the ISTQB exam

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 """
-First Day of learning ISTQB exam Chapter 1
+First Day of learning for the ISTQB exam Chapter 1
 I learnt about what is testing and test objectives
 
 """
 
 """
-Day 2  of learning ISTQB exam Chapter 1
+Day 2  of learning for the ISTQB exam 
 I Learnt about testing and debugging, why is testing necessary, testings contribution to success
 Errors, defect, failures and root causes and did some exam tests
 """
 
 """
-Day 4 of learing for ISTQB exam chapter 1
+Day 4 of learing for the ISTQB exam 
 i did a lot of exam questions like 100
 learnt about the errors and test features
 """
 
 """
-Day 5  of learing for ISTQB exam chapter 1
+Day 5  of learing for the ISTQB exam 
 I did a lot of exam questions and learned about test activities, testware and test roles
 """
 
 
 """
-Day 6  of learing for ISTQB exam chapter 1
+Day 6  of learing for the ISTQB exam 
 Just finished chapter 1 and now i have left over 100 exam questions 
 Learnt about essential skills and good practices in testing
+"""
+
+"""
+Day 7  of learing for the ISTQB exam 
+Just finished chapter 1 
+Starting chapter 2
 """
